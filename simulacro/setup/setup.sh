5bea13b97d1f6e334cda5a4cd2cdb9795ff66e9d
Álvaro <alvaro.spicot@gmail.com>
2025-05-20 12:37:35 +0200

    Cambiado setup.sh

--- simulacro/setup/setup.sh
+++ simulacro/setup/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo rpm --import https://pgp.mongodb.com/server-7.0.asc
-sudo zypper addrepo --gpgcheck "https://repo.mongodb.org/zypper/suse/15/mongodb-org/7.0/x86_64/" mongodb
+OneClickInstallUI https://software.opensuse.org/ymp/KDE:KDE3/openSUSE_Tumbleweed/openssl-1_1.ymp
+sudo rpm --import https://pgp.mongodb.com/server-8.0.asc
+sudo zypper addrepo --gpgcheck "https://repo.mongodb.org/zypper/suse/15/mongodb-org/8.0/x86_64/" mongodb
 sudo zypper -n install mongodb-org
+sudo zypper remove mongodb-mongosh
+sudo zypper install mongodb-mongosh-shared-openssl3
 sudo systemctl start mongod
